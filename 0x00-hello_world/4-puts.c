a542c1175e59f851871ba09f90274895e992ca1e
samuelifada <samifada2015@gmail.com>
2022-03-10 15:16:50 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  *  main - Prints String
+ *   *  Description: "\"Programming is like building a multilingual puzzle"
+ *    *  Return: 0
+ *    **/
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+
+}
